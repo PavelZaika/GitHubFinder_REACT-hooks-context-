cd5fd79f6fc5093f732a7005eeb1443a612b766b
PavelZaika <zpk.loki@gmail.com>
2020-04-13 15:41:42 +0300

    Refactoring app to hooks and context

---
+++ src/components/pages/About.js
@@ -0,0 +1,70 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About This App</h1>
+      <p>App to search GitHub users</p>
+      <p>Version: 1.0.0</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+        praesentium iusto deserunt modi et? Veniam deleniti aut, nobis ex
+        quibusdam vel esse pariatur laboriosam maiores ratione dolores nostrum
+        omnis beatae amet est ipsum blanditiis quia distinctio rem explicabo
+        rerum! Aspernatur dolore possimus, dolores asperiores ipsa esse totam
+        praesentium ut doloremque numquam hic assumenda quasi et alias labore
+        magnam tempora culpa veritatis quod, rerum deleniti dolor? Cupiditate,
+        ex. Necessitatibus velit earum dolorum suscipit odit ipsa, iusto
+        molestiae. Explicabo iure dolorem similique! Voluptate commodi enim
+        consectetur ullam cupiditate, vero aut provident officiis ratione. Est
+        illo consequuntur incidunt! Tenetur nihil deleniti aliquam blanditiis
+        eligendi fugit delectus sequi quod quam quasi vitae, fuga reprehenderit
+        officia vero saepe, nostrum unde! Blanditiis, saepe? Doloribus
+        consequuntur, provident praesentium culpa maxime, excepturi sequi
+        molestiae aspernatur accusantium est quaerat explicabo doloremque sit
+        aliquam facere sint nam nemo repellendus dicta velit molestias,
+        accusamus impedit repellat. Ea natus eaque autem laboriosam modi
+        inventore dolor reprehenderit porro veritatis, nisi accusantium possimus
+        nam aut non ex facere laudantium impedit illo sequi necessitatibus
+        rerum. Enim esse exercitationem laudantium aut dolor vero dolorum quia
+        sapiente odio totam aperiam perspiciatis sit, quo, vel tempora,
+        consequatur quis laboriosam. Nesciunt id culpa quasi obcaecati
+        laboriosam ipsa ipsum tempora autem deserunt. Nemo doloribus libero
+        suscipit praesentium porro aut, deleniti aspernatur voluptatem labore at
+        quibusdam voluptates natus corrupti omnis! Minus in repellat eius porro
+        veritatis qui odit incidunt, nobis sit libero natus corrupti ipsam,
+        illum rerum fuga excepturi corporis expedita, nisi quae? Id, temporibus
+        maxime qui magnam, tempore vitae quia dicta error harum dolor suscipit
+        aspernatur obcaecati quisquam fugiat itaque nisi facere deserunt. Ullam
+        cupiditate eveniet odit voluptatem blanditiis. Similique animi ducimus
+        ullam nesciunt ab minima delectus autem voluptatibus tenetur?
+        Recusandae, ipsa nemo. Id suscipit modi sunt quas placeat non ratione
+        error fugiat nulla nostrum iste in voluptate quis laboriosam alias,
+        fugit quo eligendi reprehenderit dicta repellendus qui. Culpa minus eos
+        autem dolor ipsam temporibus ducimus eveniet, quaerat saepe itaque
+        obcaecati earum libero, asperiores voluptates maxime facilis dicta id.
+        Harum maxime ab cupiditate unde impedit nesciunt quae quidem, fuga
+        necessitatibus nobis consequuntur in nemo eos, iusto repellat excepturi
+        dolorum tenetur ipsam sed, illo consectetur veritatis. Amet blanditiis
+        deleniti, vel a iusto expedita omnis ab maxime ratione unde quisquam
+        mollitia eius ea, dicta hic doloremque exercitationem tempore?
+        Voluptatum, magni sunt? Similique, aut! Sed doloribus perspiciatis illo
+        quidem earum ipsum, corporis reiciendis est dignissimos eius
+        praesentium, alias nobis nisi eaque quam dolorem obcaecati reprehenderit
+        quos facilis culpa vitae, omnis id quia? Modi ratione quo a nihil
+        placeat, alias, incidunt accusantium nesciunt necessitatibus nostrum
+        facilis blanditiis porro perferendis dolores? Dolorem tempora, quos quis
+        provident amet libero, enim temporibus asperiores odit architecto at
+        illo eligendi. Reprehenderit, magnam id mollitia voluptatum, alias
+        repellat eius ab inventore quo eum consectetur dolorum sapiente totam,
+        libero repellendus! Libero porro est deleniti natus, placeat in nemo ut
+        officiis tempora eveniet culpa odit. Amet, itaque. Eum dolorum quo nihil
+        earum. Laborum, exercitationem minus hic qui magnam facilis odit sint
+        saepe repellendus consectetur asperiores praesentium dolore magni, velit
+        numquam ex a?
+      </p>
+    </Fragment>
+  );
+};
+
+export default About;
