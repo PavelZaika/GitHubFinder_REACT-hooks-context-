c0ced36f7e73d2deff38eb933ffaa38bb57c59ff
PavelZaika <zpk.loki@gmail.com>
2020-05-06 14:13:35 +0300

    Alert Context Workflow

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Users from './components/users/Users';
@@ -8,42 +8,36 @@ import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 
 const App = () => {
-  const [alert, setAlert] = useState(null);
-
-  //Set alert
-  const showAlert = (msg, type) => {
-    setAlert({ msg, type });
-
-    setTimeout(() => setAlert(null), 5000);
-  };
-
   const NavBarTitle = 'GitHub Finder';
   return (
     <GithubState>
-      <Router>
-        <div className='App'>
-          <Navbar title={NavBarTitle} icon='fab fa-github' />
-          <div className='container'>
-            <Alert alert={alert} />
-            <Switch>
-              <Route
-                exact
-                path='/'
-                render={(props) => (
-                  <Fragment>
-                    <Search showAlert={showAlert} />
-                    <Users />
-                  </Fragment>
-                )}
-              />
-              <Route exact path='/about' component={About} />
-              <Route exact path='/user/:login' component={User} />
-            </Switch>
+      <AlertState>
+        <Router>
+          <div className='App'>
+            <Navbar title={NavBarTitle} icon='fab fa-github' />
+            <div className='container'>
+              <Alert />
+              <Switch>
+                <Route
+                  exact
+                  path='/'
+                  render={(props) => (
+                    <Fragment>
+                      <Search />
+                      <Users />
+                    </Fragment>
+                  )}
+                />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/user/:login' component={User} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </AlertState>
     </GithubState>
   );
 };
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,18 +1,24 @@
 import React, { useState, useContext } from 'react';
-import PropTypes from 'prop-types';
-import GithubContext from '../../context/github/githubContext';
 
-const Search = ({ showAlert }) => {
+import GithubContext from '../../context/github/githubContext';
+import AlertContext from '../../context/alert/alertContext';
+
+const Search = () => {
   const githubContext = useContext(GithubContext);
+  const alertContext = useContext(AlertContext);
+
+  const { setAlert } = alertContext;
+
+  const { searchUsers, clearUsers } = GithubContext;
 
   const [text, setText] = useState('');
 
   const onSubmit = (e) => {
     e.preventDefault();
     if (text === '') {
-      showAlert('Please enter something', 'light');
+      setAlert('Please enter something', 'light');
     } else {
-      githubContext.searchUsers(text);
+      searchUsers(text);
       setText('');
     }
   };
@@ -38,17 +44,12 @@ const Search = ({ showAlert }) => {
         />
       </form>
       {githubContext.users.length > 0 && (
-        <button
-          className='btn btn-light btn-block'
-          onClick={githubContext.clearUsers}
-        >
+        <button className='btn btn-light btn-block' onClick={clearUsers}>
           Clear
         </button>
       )}
     </div>
   );
 };
-Search.propTypes = {
-  showAlert: PropTypes.func.isRequired,
-};
+
 export default Search;
